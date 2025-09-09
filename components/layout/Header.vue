0d8de04acc6b544cd2f1b84a38e262d003c4819a
Cristian Escalante <160914031+Cristian3ska@users.noreply.github.com>
2025-09-09 13:11:22 -0600

    Integracion de header, description y carrusel de logos

---
+++ components/layout/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="appheader">
+        <div class="logo">
+            <img :src="logo" alt="Logo">
+        </div>
+
+        <nav class="nav-links">
+            <a href="#">Inicio</a>
+            <a href="#">Servicios</a>
+            <a href="#">Contacto</a>
+            <button href="">Registro</button>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import logo from '/assets/images/logo.svg'
+import '@/assets/css/styles/Header.css'
+
+onMounted(() => {
+    const header = document.querySelector('.appheader')
+
+    const handleScroll = () => {
+        if (window.scrollY > window.innerHeight) {
+            header.classList.add('scrolled')
+        } else {
+            header.classList.remove('scrolled')
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <!-- pages/index.vue -->
 <template>
   <main>
+    <Header />
     <Hero />
+    <Description />
+    <LogosUni />
   </main>
 </template>
 
 <script setup>
-import Hero from '~/components/sections/Hero.vue'
+  import Hero from '~/components/sections/Hero.vue'
+  import Header from '~/components/layout/Header.vue'
+  import  Description from '~/components/sections/Description.vue'
+  import LogosUni from '~/components/sections/Logos-uni.vue'
 </script>
