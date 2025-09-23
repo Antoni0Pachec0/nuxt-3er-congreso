34892e71b1b0393dd204545a8a6d229d148493c5
Cristian Escalante <160914031+Cristian3ska@users.noreply.github.com>
2025-09-23 10:31:49 -0600

    fix date

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -90,10 +90,10 @@
 <script setup>
 // CAMBIO: Importar 'ref' de vue
 import { onMounted, onUnmounted, reactive, ref } from 'vue'
-import '~/assets/css/styles/hero.css'
+import '~/assets/css/styles/Hero.css'
 
 // Fecha del evento
-const target = new Date('2025-09-12T09:00:00')
+const target = new Date('2025-11-12T09:00:00')
 
 const timeLeft = reactive({ days: '00', hours: '00', minutes: '00', seconds: '00' })
 let timer
