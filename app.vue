22ef8c3d63dfaa5553e3fb8b816948ec1013d18a
vanyx2002 <yovannycorteslezama2018@gmail.com>
2025-10-05 15:01:10 -0600

    animacion de carga

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <!-- app.vue -->
 <template>
   <div>
+    <LoadingOverlay />
+
     <Header />
     <NuxtPage />
     <Footer />
@@ -10,4 +12,6 @@
 <script setup>
 import Header from "@/components/layout/Header.vue";
 import Footer from "@/components/layout/Footer.vue";
+import LoadingOverlay from "./components/LoadingOverlay.vue";
+
 </script>
